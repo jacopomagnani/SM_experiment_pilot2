7c356c8f0c0d8751b81b35e568bc194da2950020
jacopomagnani2 <Jacopo@ADUAED07690LPMX.local>
2018-11-13 14:28:18 +0400

    updated player.side to take values in {1,2},  in Practice app

--- Practice/models.py
+++ Practice/models.py
@@ -19,6 +19,7 @@ class Constants(BaseConstants):
     num_rounds = 1
     part1_end = 1
     type_space = [2, 10]
+    side_space = [1, 1, 1, 2, 2, 2]
 
 
 class Subsession(BaseSubsession):
@@ -40,29 +41,29 @@ class Player(BasePlayer):
 
     def initialize_player(self):
         self.type = round(random.uniform(Constants.type_space[0], Constants.type_space[1]),1)
-        self.side = 0
+        self.side = 1
 
     def get_outcome(self):
         me_list = [1, 0, 0, 0, 0, 0]
-        side_list = [0, 0, 0, 1, 1, 1]
-        type_list = [round(random.uniform(Constants.type_space[0], Constants.type_space[1]), 1) for i in range(6)]
+        side_list = Constants.side_space
+        type_list = [round(random.uniform(Constants.type_space[0], Constants.type_space[1]), 1) for _ in range(6)]
         type_list[0] = self.type
-        bid_list = [random.uniform(0, 10) for i in range(6)]
+        bid_list = [random.uniform(0, 10) for _ in range(6)]
         bid_list[0] = self.bid
         rank_list = [0, 0, 0, 0, 0, 0]
-        side0_bids = [bid_list[i] for i in range(6) if side_list[i] == 0]
         side1_bids = [bid_list[i] for i in range(6) if side_list[i] == 1]
-        side0_bids.sort()
+        side2_bids = [bid_list[i] for i in range(6) if side_list[i] == 2]
         side1_bids.sort()
-        for b in set(side0_bids):
-            rank_set = [i for i, x in enumerate(side0_bids) if x == b]
-            for i in range(6):
-                if side_list[i] == 0 and bid_list[i] == b:
-                    rank_list[i] = rank_set.pop(random.randrange(0, len(rank_set)))
+        side2_bids.sort()
         for b in set(side1_bids):
             rank_set = [i for i, x in enumerate(side1_bids) if x == b]
             for i in range(6):
                 if side_list[i] == 1 and bid_list[i] == b:
+                    rank_list[i] = rank_set.pop(random.randrange(0, len(rank_set)))
+        for b in set(side2_bids):
+            rank_set = [i for i, x in enumerate(side2_bids) if x == b]
+            for i in range(6):
+                if side_list[i] == 2 and bid_list[i] == b:
                     rank_list[i] = rank_set.pop(random.randrange(0, len(rank_set)))
         self.rank = rank_list[0]
         self.participant.vars['practice_data'] = [{"id": me_list[i], "side": side_list[i], "type": type_list[i], "bid": bid_list[i], "rank": rank_list[i]} for i in range(6)]
@@ -73,4 +74,3 @@ class Player(BasePlayer):
                 self.partner_type = q['type']
                 self.points = self.type * q['type'] - self.bid
         self.points = round(self.points, 1)
-        self.participant.vars['j'] = 1

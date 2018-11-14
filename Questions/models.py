9123958b45ad46edd7e565f591257f1f168e3e0f
jacopomagnani2 <Jacopo@ADUAED07690LPMX.local>
2018-11-14 13:45:13 +0400

    udpated questions app with asymmetric examples

--- Questions/models.py
+++ Questions/models.py
@@ -28,14 +28,14 @@ class Group(BaseGroup):
 class Player(BasePlayer):
     q1 = models.FloatField()
     q2 = models.IntegerField(choices=[
-        [1, 'Player B will earn 10 points.'],
-        [2, 'Player B will earn either 10 points or 14 points.'],
-        [3, 'Player B may earn any of the following amounts of points: 10, 14 or 4.'],
+        [1, 'Player B will earn 7 points.'],
+        [2, 'Player B will earn either 7 points or 1 point.'],
+        [3, 'Player B may earn any of the following amounts of points: 7, 1 or -3.'],
         ], widget=widgets.RadioSelect
     )
     q3 = models.IntegerField(choices=[
-        [1, 'Player B will earn 0 points.'],
-        [2, 'Player B will earn either 0 points or 6 points.'],
-        [3, 'Player B may earn any of the following amounts of points: 0, 6 or 24.'],
+        [1, 'Player B will earn 12 points.'],
+        [2, 'Player B will earn either 12 points or 4 points.'],
+        [3, 'Player B may earn any of the following amounts of points: 12, 4 or -8.'],
         ], widget=widgets.RadioSelect
-)
+    )

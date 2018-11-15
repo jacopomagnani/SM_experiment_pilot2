f232e76aaa86dc838185c47b613d278e4bc77752
jacopomagnani2 <Jacopo@ADUAED07690LPMX.local>
2018-11-15 13:33:31 +0400

    restored full app sequence

--- settings.py
+++ settings.py
@@ -20,8 +20,7 @@ SESSION_CONFIGS = [
         'name': 'configs',
         'display_name': "My Configs",
         'num_demo_participants': 5,
-        #'app_sequence': ['instructions', 'Practice', 'Questions', 'Game', 'finalpage']
-        'app_sequence': [ 'Game', 'finalpage']
+        'app_sequence': ['instructions', 'Practice', 'Questions', 'Game', 'finalpage']
     }
     ]
 # see the end of this file for the inactive session configs
